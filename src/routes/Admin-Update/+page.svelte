f2c6038b1b470302167febdfc0826fe145b1afdf
Acedelacuesta <Acedelacuesta30@gmail.com>
2023-05-24 18:31:50 +0800

    half update of AdminStudent Update Module

--- src/routes/Admin-Update/+page.svelte
+++ src/routes/Admin-Update/+page.svelte
@@ -1,3 +1,103 @@
+<script>
+	import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+	import { goto } from '$app/navigation';
+	import { firebase, firestore, functions } from '$lib/firebase';
+	import { getDatabase, ref, onValue, get, set, remove } from 'firebase/database';
+	import { auth, database } from '$lib/firebase';
+	import { onMount } from 'svelte';
+	import { query, collection, where, getDocs } from 'firebase/firestore';
+	import { updateDoc, arrayUnion, doc, arrayRemove } from 'firebase/firestore';
+
+	let studentNumber;
+	let rfidTag;
+	let studentInfo = [];
+	let studentGetRFID;
+
+	// Create a reference to the 'users' collection
+	async function studentUpdate() {
+		const collectionRef = collection(firestore, 'users');
+		const queryRef = query(collectionRef, where('studentID', '==', studentNumber));
+		const querySnapshot = await getDocs(queryRef);
+		querySnapshot.forEach((doc) => {
+			const docData = doc.data();
+			console.log(docData);
+
+			if (docData.hasOwnProperty('studentRFID')) {
+				studentGetRFID = docData.studentRFID;
+			}
+
+			// Use the `studentRFID` variable as needed
+			console.log(studentGetRFID);
+		});
+
+		const tasksRef = ref(database, 'rfid/' + studentGetRFID);
+
+		remove(tasksRef).then(() => {
+			console.log('location removed');
+		});
+
+		set(ref(database, 'rfid/' + rfidTag), rfidTag);
+		studentClassUpdate();
+		studentSubjectUpdate();
+	}
+
+	async function studentClassUpdate() {
+		const collectionRef = collection(firestore, 'classes');
+		const queryRef = query(collectionRef, where('students', 'array-contains', studentGetRFID));
+		const querySnapshot = await getDocs(queryRef);
+
+		querySnapshot.forEach((document) => {
+			console.log('Matching document:', document.data());
+
+			const docRef = doc(firestore, 'classes', document.id);
+			const studentsArray = document.data().students;
+			const updatedStudents = studentsArray.filter((student) => student !== studentGetRFID);
+			const updatedStudentsWithNewValue = [...updatedStudents, rfidTag];
+			const updateData = {
+				students: updatedStudentsWithNewValue
+			};
+
+			updateDoc(docRef, updateData)
+				.then(() => {
+					console.log('Element removed and new value inserted successfully');
+				})
+				.catch((error) => {
+					console.error('Error removing element and inserting new value:', error);
+				});
+		});
+	}
+
+	async function studentSubjectUpdate() {
+		const collectionRef = collection(firestore, 'Subject');
+		const queryRef = query(collectionRef, where('students', 'array-contains', studentGetRFID));
+		const querySnapshot = await getDocs(queryRef);
+
+		querySnapshot.forEach((document) => {
+			console.log('Matching document:', document.data());
+
+			const docRef = doc(firestore, 'Subject', document.id);
+			const studentsArray = document.data().students;
+			const updatedStudents = studentsArray.filter((student) => student !== studentGetRFID);
+			const updatedStudentsWithNewValue = [...updatedStudents, rfidTag];
+			const updateData = {
+				students: updatedStudentsWithNewValue
+			};
+
+			updateDoc(docRef, updateData)
+				.then(() => {
+					console.log('Element removed and new value inserted successfully');
+				})
+				.catch((error) => {
+					console.error('Error removing element and inserting new value:', error);
+				});
+		});
+	}
+
+	async function studentRecitationUpdate() {
+		
+	}
+</script>
+
 <header class="w-full text-gray-700 bg-white border-t border-gray-100 shadow-sm body-font">
 	<div class="container flex flex-col flex-wrap items-center pt-2 mx-auto md:flex-row">
 		<button>
@@ -26,28 +126,32 @@
 					<!--ADD CLASS-->
 					<div class="container mx-auto">
 						<div class="max-w-md mx-auto">
-							<div class="text-center"/>
+							<div class="text-center" />
 							<div class="m-7">
 								<div class="mb-6">
 									<label for="class" class="block mb-6 text-lg font-medium text-gray"
 										>Update Student Data</label
 									>
 									<input
+										bind:value={studentNumber}
 										type="name"
 										name="gradelevel"
 										id="grade"
-										placeholder="Student Number"
+										placeholder="Select Student Number"
 										class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none"
 									/>
+
 									<input
+										bind:value={rfidTag}
 										type="name"
 										name="classname"
 										id="classname"
-										placeholder="RFID Tag"
+										placeholder="Update RFID Tag"
 										class="mt-3 w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none"
 									/>
 								</div>
 								<button
+									on:click={studentUpdate}
 									class=" w-full px-3 py-4 text-white rounded-md bg-[#2ea44f] focus:outline-none"
 									>Update</button
 								>
